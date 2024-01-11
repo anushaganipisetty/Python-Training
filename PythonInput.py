b126cec448ef1bfa35d8dc846620028444345c6d
ANUSHA GANIPISETTY <140327203+anushaganipisetty@users.noreply.github.com>
2024-01-11 10:57:44 -0600

    Update PythonInput.py

--- PythonInput.py
+++ PythonInput.py
@@ -2,3 +2,6 @@
 # THEN PRINT() WILL PRINT THE WORLD "HELLO" AND NAME
 input("What is your name?")
 print("Hello" +input("What is your name?"))
+
+#len(input())
+print(len(input("What is your name?")))
