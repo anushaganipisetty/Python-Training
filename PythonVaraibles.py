a8193b7e20977c3268902ec0d1f2c420d24afd57
ANUSHA GANIPISETTY <140327203+anushaganipisetty@users.noreply.github.com>
2024-01-11 11:53:59 -0600

    Add files via upload

---
+++ PythonVaraibles.py
@@ -0,0 +1,12 @@
+james = 1234567890
+print(james)
+
+name = 'Jack'
+print(name)
+
+print(len(input("What is your name")))
+
+print()
+name = input("what is your name?")
+length = len(name)
+print(length)
